60bebf8ec20fbca9733e5e8931fad3d431f45a97
Alanis Castro <74870719+alaniscastro@users.noreply.github.com>
2022-08-29 15:19:07 -0300

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -2,10 +2,10 @@
 
 args=("$@")
 
-FILE_NAME=${args[0]} 		#Path to the file
+FILE_NAME=${args[0]} 		  #Path to the file
 FINAL_FILE=${args[1]} 		#Path to the file and the name you want to give it
-OLD_STRING=${args[2]}	#The string you want to replace
-NEW_STRING=${args[3]}	#What you want to replace the string with
+OLD_STRING=${args[2]}	    #The string you want to replace
+NEW_STRING=${args[3]}   	#What you want to replace the string with
 
 #Creating the file to export the data
         touch $FINAL_FILE
@@ -14,16 +14,16 @@ echo "Replacing '$OLD_STRING' for '$NEW_STRING' globally in $FILE_NAME"
 echo ""
 
 while read line; do
-      for word in $line; do
-      a=" "
-      echo "$word"
-      echo""
-            if [ $OLD_STRING == $word ]; then
-                  a+=$NEW_STRING
-                  echo -n "$a" >> $FINAL_FILE
-            else
-                  a+=$word
-                  echo -n "$a" >> $FINAL_FILE
-            fi
-    done
+        a=""
+        for word in $line; do
+                if [ $OLD_STRING == $word ]; then
+                        a+=$NEW_STRING
+                        echo -n "$a" >> $FINAL_FILE
+                else
+                        a+=$word
+                        echo -n "$a" >> $FINAL_FILE
+                fi
+        a=" "
+        done
+        echo "" >> $FINAL_FILE
 done < $FILE_NAME
